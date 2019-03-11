65752581a858c1cd6da6e9c8b5bbe6df542291b1
jonismith <jonathan.pizarro@protecso.com.pe>
2019-03-11 18:04:35 -0500

    second changue

--- src/main/java/com/invpersona/crudPersona/CrudPersonaApplication.java
+++ src/main/java/com/invpersona/crudPersona/CrudPersonaApplication.java
@@ -2,6 +2,10 @@ package com.invpersona.crudPersona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CrudPersonaApplication {
@@ -9,5 +13,15 @@ public class CrudPersonaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudPersonaApplication.class, args);
 	}
-
+  
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("POST", "GET", "PUT", "DELETE", "HEAD").allowedOrigins("*")
+						.allowCredentials(false);
+			}
+		};
+    }
 }
